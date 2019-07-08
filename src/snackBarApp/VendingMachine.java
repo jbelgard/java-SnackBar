5ea79ba0587d407abf4a4aa8bb07431159c717f7
Jason Belgard <belgard.jason@gmail.com>
2019-07-08 17:14:56 -0500

    another update

--- src/snackBarApp/VendingMachine.java
+++ src/snackBarApp/VendingMachine.java
@@ -5,4 +5,27 @@ public class VendingMachine
 	private int maxId = 0;
 	private int id;
 	private String name;
+
+	public VendingMachine(String name)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
 }
